07fa486562acbf5a1b8a344a19188562ae8be968
Mafiuz04 <mathew.modrzynski@gmailc.om>
2024-07-19 16:14:34 +0200

    nbp

---
+++ src/test/java/com/Mafiuz04/nbp/NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mafiuz04.nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
